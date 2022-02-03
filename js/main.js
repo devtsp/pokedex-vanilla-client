642917cf6d058baff1f0ae8e920584557bf768dd
Tomas <87679143+devtsp@users.noreply.github.com>
2022-02-03 01:19:37 -0300

    info card
    main info correctly displayed on card when click

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
-API_URL = 'https://pokeapi.co/api/v2/pokemon';
-const $index = document.querySelector('#index');
+API_URL = 'https://pokeapi.co/api/v2';
+
+const q = str => {
+	return document.querySelector(str);
+};
+
+const $index = q('#index');
 const $pokeCards = document.querySelectorAll('.poke-card');
-const $previousPage = document.querySelector('#previous-page');
-const $nextPage = document.querySelector('#next-page');
-const $pokeInfo = document.querySelector('#pokemon-info');
+const $previousPage = q('#previous-page');
+const $nextPage = q('#next-page');
+const $pokeInfo = q('#pokemon-info');
 let paginationPrevious = '';
 let paginationNext = '';
 const cacheName = 'v1';
@@ -16,7 +21,7 @@ const fetchData = URL => {
 
 const handleMatch = (request, match, cache, callback) => {
 	if (!match) {
-		// console.log('Not in cache! proceeding to fetch and clone');
+		// console.log('Not in cache: ', request);
 		fetchData(request)
 			.then(response => {
 				// console.log(response);
@@ -31,7 +36,7 @@ const handleMatch = (request, match, cache, callback) => {
 			})
 			.catch(err => console.log(err));
 	} else {
-		// console.log('Found in cache!');
+		// console.log('Found in cache: ', match.url);
 		cache
 			.match(request)
 			.then(response => {
@@ -56,7 +61,8 @@ const displayChanges = object => {
 		const spriteUrl = Object.values(object[index]);
 		$pokeCards[index].id = pokemonName;
 		$pokeCards[index].children[1].innerText = pokemonName;
-		$pokeCards[index].children[0].setAttribute('src', spriteUrl);
+		$pokeCards[index].children[0].src = spriteUrl;
+		$pokeCards[index].children[0].alt = pokemonName;
 	}
 };
 
@@ -71,7 +77,7 @@ const displayPokemonCards = data => {
 	});
 	const sprites = {};
 	pokemons.forEach((pokemon, i) => {
-		handleRequest(`${API_URL}/${pokemon}`, data => {
+		handleRequest(`${API_URL}/pokemon/${pokemon}`, data => {
 			const spriteUrl = data.sprites.front_default;
 			const pair = {};
 			pair[pokemon] = spriteUrl;
@@ -79,6 +85,31 @@ const displayPokemonCards = data => {
 			Object.entries(sprites).length == 12 && displayChanges(sprites);
 		});
 	});
+};
+
+const setInfoCard = pokemon => {
+	handleRequest(`${API_URL}/pokemon/${pokemon}`, pokemon => {
+		// console.log(pokemon);
+		q('#name').innerText = pokemon.name;
+		q('#type').innerText = pokemon.types[0].type.name;
+		q('#main-pic').src =
+			pokemon.sprites.other['official-artwork']['front_default'];
+		let abilities = [];
+		pokemon.abilities.forEach(ability => {
+			abilities.push(ability.ability.name);
+		});
+		q('#abilities').innerText = abilities.join(', ');
+	});
+	handleRequest(`${API_URL}/pokemon-species/${pokemon}`, specie => {
+		q('#habitat').innerText = specie.habitat.name;
+		handleRequest(specie.evolution_chain.url, evolutionChain => {
+			// console.log(evolutionChain);
+			q('#evolution').innerText =
+				evolutionChain.chain.evolves_to[0]?.species?.name || '-';
+		});
+	});
+
+	$pokeInfo.classList.remove('visually-hidden');
 };
 
 $previousPage.onclick = e => {
@@ -95,8 +126,15 @@ $pokeInfo.querySelector('i').onclick = e => {
 
 $index.onclick = e => {
 	if (e.target.parentNode.classList.contains('poke-card')) {
-		$pokeInfo.classList.remove('visually-hidden');
+		q('#name').src = '';
+		q('#type').innertText = '';
+		q('#pokemon-info img').src = 'img/pokeball.png';
+		q('#pokemon-info img').alt = e.target.parentNode.id;
+		q('#abilities').innerText = '';
+		q('#habitat').innerText = '';
+		q('#evolution').innerText = '';
+		setInfoCard(e.target.parentNode.id);
 	}
 };
 
-handleRequest(`${API_URL}?limit=12`, displayPokemonCards);
+handleRequest(`${API_URL}/pokemon?limit=12`, displayPokemonCards);
