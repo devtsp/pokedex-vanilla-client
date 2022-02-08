959fb45caf0f3fd101ac5260969e581ab595c4fb
Tomas <87679143+devtsp@users.noreply.github.com>
2022-02-08 19:51:29 -0300

    Cache module
    removed unwanted logs

--- js/cache_version.js
+++ js/cache_version.js
@@ -1,14 +1,11 @@
 export const checkCacheVersion = () => {
+	const date = Date.now();
 	if (!localStorage.getItem('cache-version')) {
-		const date = Date.now();
-		console.log(date);
 		localStorage.setItem('cache-version', date);
 	} else {
 		if (localStorage.getItem('cache-version') <= Date.now() - 86400000) {
 			caches.delete(localStorage.getItem('cache-version'));
 			localStorage.removeItem('cache-version');
-			const date = Date.now();
-			console.log(date);
 			localStorage.setItem('cache-version', date);
 		}
 	}
