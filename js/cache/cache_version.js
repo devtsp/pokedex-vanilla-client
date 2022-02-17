e09cfa608b07fdb63dfbbb30eedc7522a4a032f1
Tomas <87679143+devtsp@users.noreply.github.com>
2022-02-17 17:44:04 -0300

    cleaner cache versioning function

--- js/cache/cache_version.js
+++ js/cache/cache_version.js
@@ -1,12 +1,16 @@
 export const checkCacheVersion = () => {
-	const date = Date.now();
-	if (!localStorage.getItem('cache-version')) {
-		localStorage.setItem('cache-version', date);
+	const cacheName = 'pokedex-cache-version';
+	const actualDate = Date.now();
+	const actualCache = localStorage.getItem(cacheName);
+	if (!actualCache) {
+		localStorage.setItem(cacheName, actualDate);
 	} else {
-		if (localStorage.getItem('cache-version') <= date - 86400000) {
-			caches.delete(localStorage.getItem('cache-version'));
-			localStorage.removeItem('cache-version');
-			localStorage.setItem('cache-version', date);
+		const dayInMs = 86400000;
+		const isFresh = actualCache + dayInMs < actualDate;
+		if (!isFresh) {
+			caches.delete(actualCache);
+			localStorage.removeItem(cacheName);
+			localStorage.setItem(cacheName, actualDate);
 		}
 	}
 };
